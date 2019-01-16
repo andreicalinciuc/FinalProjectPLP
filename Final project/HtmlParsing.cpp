9da0326396a22f5aa9929a7c1870903a28cfdbef
andreicalinciuc <calinciuc.andrei@yahoo.com>
2019-01-16 15:22:32 +0200

    kind of finally

--- Final project/HtmlParsing.cpp
+++ Final project/HtmlParsing.cpp
@@ -9,11 +9,13 @@
 #include <algorithm>
 
 using namespace std;
-struct elements{
+struct elements {
     string tag;
     string str;
 
-}e[5000];
+} e[5000];
+
+
 string ReplaceAll(std::string str, const std::string &from, const std::string &to) {
     size_t start_pos = 0;
     while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
@@ -22,21 +24,18 @@ string ReplaceAll(std::string str, const std::string &from, const std::string &t
     }
     return str;
 }
-string GetTag(char tag[1024]){
-    char name_tag[1024]="\0";
-    int    size=0;
-    bzero(name_tag,1024);
+
+string GetTag(char tag[1024]) {
+    char name_tag[1024] = "\0";
+    int size = 0;
+    bzero(name_tag, 1024);
 
     for (int i = 0; i < strlen(tag); i++)
         if (tag[0] == '/') {
-            if (tag[i] != ' ')
-            {
-                if(tag[i]!='/' && tag[i]!='<' && tag[i]!='>' && tag[i]!=':')
-                    name_tag[size++]=tag[i];
+            if (tag[i] != ' ' && tag[i]!=':') {
+                name_tag[size++] = tag[i];
 
-            }
-            else
-            {
+            } else {
 
                 break;
             }
@@ -44,31 +43,32 @@ string GetTag(char tag[1024]){
     return name_tag;
 }
 
-string GetValues(char lines[1024])
-{
-    char name_tag[1024]="\0";
-    int    size=0;
-    bzero(name_tag,1024);
-    int poz_rec=0;
-    for (int i = 0; i < strlen(lines); i++)
-    {
-        if(lines[i]==' ' || lines[i]=='<')
-        {
-            poz_rec=i;
+string GetValues(char lines[1024]) {
+    char name_tag[1024] = "\0";
+    int size = 0;
+    bzero(name_tag, 1024);
+    int poz_rec = 0;
+    for (int i = 0; i < strlen(lines); i++) {
+        if (lines[i] == ' ' || lines[i] == '<') {
+
+            poz_rec = i;
             break;
         }
 
+    }
+    if (lines[poz_rec + 1] == '-') {
+        return name_tag;
 
     }
 
     for (int i = poz_rec; i < strlen(lines); i++)
-        cout<<lines[i];
+        name_tag[size++] = lines[i];
 
 
-
-        return name_tag;
+    return name_tag;
 
 }
+
 void HtmlParsing::ReadFile() {
     FILE *fp = fopen("../date.txt", "r");
     if (fp == NULL)
@@ -76,13 +76,21 @@ void HtmlParsing::ReadFile() {
     char *list = NULL;
     char *tag = NULL;
     size_t len = 0;
-    int cnt=0;
+    int cnt = 0;
+    ofstream myfile;
+    myfile.open("../dates.txt");
     while ((getline(&list, &len, fp)) != -1) {
 
         strcat(line, list);
         tag = list;
-        e[cnt].tag=GetTag(tag);
-        e[cnt++].str=GetValues(list);
+        e[cnt].tag = GetTag(tag);
+        if (e[cnt].tag.size() > 0)
+            myfile << e[cnt].tag << endl;
+
+        e[cnt].str = GetValues(list);
+        myfile << e[cnt].str << endl;
+
+        cnt++;
 
 
     }
@@ -91,31 +99,57 @@ void HtmlParsing::ReadFile() {
 
 
 void HtmlParsing::CheckComand() {
-
-    char *str = line;
-    char *token = strtok(str, "\n");
     ofstream myfile;
     myfile.open("../test.html");
     myfile << "<!DOCTYPE html>\n" << endl <<
            "<html>\n" << endl <<
-           "<head>" << endl << "<link rel=\"stylesheet\" type=\"text/css\" href=\"test.css\">"
+           "<head>" << endl << "<link rel=\"stylesheet\" type=\"text/css\" href=\"Design.css\">"
 
                                "</head>" << endl <<
            "<body>" << endl;
-    myfile << "<pre>";
-    while (token) {
-        string str = ReplaceAll(token, "<", "&lt;");
-        str = ReplaceAll(str, "/>", "/&gt;");
 
-        myfile << str << endl;
-        token = strtok(NULL, "\n");
+    ifstream file("../dates.txt");
+    if (file.is_open()) {
+        string line;
+        int h1=0;
+        int h3=0;
+        int div=1;
+        while (getline(file, line)) {
+
+
+            string r = line.substr(0, 3);
+            if (r == "///") {
+                h3=1;
+                div=0;
+                myfile << "<h1>" << line.substr(3, line.length()) << "</h1>" << endl;
+            } else if (r == "//-") {
+                div=0;
+                myfile << "<h3>" << line.substr(3, line.length()) << "</h3>" << endl;
+            } else if(line.size()!=0){
+                if(h3!=1){
+                    string str = ReplaceAll(line, "<", "&lt;");
+                    str = ReplaceAll(str, "/>", "/&gt;");
+                    myfile << "<pre>" << str << "</pre>" << endl;
+                }
+                else
+                {
+                    h3=0;
+                    string str = ReplaceAll(line, "<", "&lt;");
+                    str = ReplaceAll(str, "/>", "/&gt;");
+                    myfile << "<h4>" << str << "</h4>" << endl;
+                }
+
+            }
+        }
+        file.close();
     }
-    myfile << "</pre>";
 
     myfile << "\n"
               "</body>\n" << endl <<
            "</html>" << endl;
     myfile.close();
+
+
 }
 
 
